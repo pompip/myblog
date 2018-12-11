121a0ff691c91b8f62e3ec41419cbdba1a6333e7
pompip <pompip@163.com>
2018-12-11 22:30:20 +0800

    second datasource

--- src/main/java/cn/pompip/myblog/config/DatasourceConfig.java
+++ src/main/java/cn/pompip/myblog/config/DatasourceConfig.java
@@ -1,30 +1,30 @@
-//package cn.pompip.myblog.config;
-//
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import javax.sql.DataSource;
-//
-//@Configuration
-//class DatasourceConfig {
-//
-//
-//    @Bean("primaryDataSource")
-//    @Primary
-//    @Qualifier("primaryDataSource")
-//    @ConfigurationProperties(prefix = "spring.primary.datasource")
-//    DataSource primaryDatasource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean("secondaryDataSource")
-//    @Qualifier("secondaryDataSource")
-//    @ConfigurationProperties(prefix = "spring.secondary.datasource")
-//    DataSource secondaryDataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//}
-//
+package cn.pompip.myblog.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import javax.sql.DataSource;
+
+@Configuration
+class DatasourceConfig {
+
+
+    @Bean("primaryDataSource")
+    @Primary
+    @Qualifier("primaryDataSource")
+    @ConfigurationProperties(prefix = "pompip.primary.datasource")
+    DataSource primaryDatasource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @ConfigurationProperties(prefix = "pompip.secondary.datasource")
+    DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
+
