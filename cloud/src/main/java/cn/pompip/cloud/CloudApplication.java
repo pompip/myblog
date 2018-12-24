b181b714961063399ab47c5e5f5b455975857d36
pompip <pompip@163.com>
2018-12-25 00:05:06 +0800

    move boot module to blog

---
+++ cloud/src/main/java/cn/pompip/cloud/CloudApplication.java
@@ -0,0 +1,15 @@
+package cn.pompip.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@EnableEurekaServer
+public class CloudApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CloudApplication.class,args);
+    }
+}
