6ba2e97d35a14a0fb446b257b0648d3e1c7f400d
pompip <pompip@163.com>
2019-01-30 17:53:13 +0800

    add mybatis test

--- blog/src/main/java/cn/pompip/myblog/MyBlogApplication.java
+++ blog/src/main/java/cn/pompip/myblog/MyBlogApplication.java
@@ -1,15 +1,10 @@
 package cn.pompip.myblog;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
-//@EntityScan("cn.pompip.lib")
-//@EnableJpaRepositories(basePackages = "cn.pompip.lib.dao")
 @SpringBootApplication
 @EnableDiscoveryClient(autoRegister = false)
 @MapperScan("cn.pompip.myblog.mapper")
